0dd4eea0bba7307a0a3ebf1570a07fbf593f6524
Sita <sitavemuri8@gmail.com>
2023-06-30 21:58:39 -0700

    Initial Commit

---
+++ NutritiousPromotional/NutritiousPromotionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritiousPromotionalApp.swift
+//  NutritiousPromotional
+//
+//  Created by Sita on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritiousPromotionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
