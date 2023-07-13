094d77675a7384919d7a37ef58195fcff18f3625
Sita <sitavemuri8@gmail.com>
2023-07-12 18:57:23 -0700

    finish video and q&a

--- NutritiousPromotional/ContentView.swift
+++ NutritiousPromotional/ContentView.swift
@@ -8,26 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var slides: [String] = ["estee 47", "estee 48"]
+    @State var slide = false
+    
+    init() {
+        UIToolbar.appearance().setBackgroundImage(UIImage(),
+                                        forToolbarPosition: UIBarPosition.any,
+                                        barMetrics: UIBarMetrics.default)
+        UIToolbar.appearance().setShadowImage(UIImage(),
+                                    forToolbarPosition: UIBarPosition.any)
+    }
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Image("Estee_Lauder_Logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal, 100)
-                
-            }
-            .toolbar {
-                ToolbarItemGroup(placement: .status) {
-                Link("About", destination: URL(string: "https://www.esteelauder.com/nutritious-skincare-benefits")!)
-                    .padding(.horizontal, 30)
-                    .padding(.vertical, 10)
-                    .foregroundColor(.white)
-                    .background(Rectangle()
-                        .foregroundColor(.black))
-                    .cornerRadius(25)
+            ScrollView {
+                VStack {
+                    Button {
+                        slide.toggle()
+                    } label: {
+                        Image(slide == true ? slides[1] : slides[0])
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.leading, .bottom, .trailing])
+                            .id(slide)
+                            .transition(.opacity.animation(.easeOut))
+                    }
+                    Video()
                 }
             }
+            .toolbar {
+                ToolbarItemGroup(placement: .principal) {
+                    Image("Estee_Lauder_Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal, 100)
+                }
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Link("Store", destination: URL(string: "https://www.esteelauder.com/nutritious-skincare-benefits")!)
+                        .padding(.horizontal, 30)
+                        .padding(.vertical, 10)
+                        .foregroundColor(.white)
+                        .background(Rectangle()
+                            .foregroundColor(.black))
+                        .cornerRadius(25)
+                }
+            }
+            .toolbarBackground(.hidden, for: .navigationBar)
         }
     }
 }
--- NutritiousPromotional/Video.swift
+++ NutritiousPromotional/Video.swift
@@ -11,39 +11,35 @@ import AVKit
 struct Video: View {
     @State var player = AVPlayer(url: Bundle.main.url(forResource: "SP23_Nutritious_Hero_Campaign_Nutrigenomics_20s_1920x1920_Sound_Super_GLOBAL_COMP", withExtension: "mp4")!)
     @State private var scrollPosition: CGPoint = .zero
+    @Namespace var vid
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack {
-//                    ForEach((1...20), id: \.self) { row in
-//                        Text("Row \(row)")
-//                            .frame(height: 30)
-//                            .id(row)
-//                    }
-                    VideoPlayer(player: player)
-                        .scaledToFit()
-                    let _ = player.isMuted = true
-                }
-                .background(GeometryReader { geometry in
-                    Color.clear
-                        .preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scroll")).origin)
-                })
-                .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
-                    if self.scrollPosition.y > value.y {
-                        player.rate = 1.0
-                        player.pause()
-                    } else if self.scrollPosition.y < value.y {
-                        player.rate = -1.0
-                        player.pause()
+            ScrollViewReader { proxy in
+                ScrollView {
+                    VStack {
+                        VideoPlayer(player: player)
+                            .frame(width: 360, height: 360)
+                        let _ = player.isMuted = true
                     }
-                    self.scrollPosition = value
+                    .id(vid)
+                    .background(GeometryReader { geometry in
+                        Color.clear
+                            .preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scroll")).origin)
+                    })
+                    .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+                        if self.scrollPosition.y > value.y {
+                            player.rate = 1.0
+                            player.pause()
+                            proxy.scrollTo(vid)
+                        }
+                        self.scrollPosition = value
+                    }
+                    Spacer()
+                        .frame(height: 1)
                 }
+                .frame(height: 360)
             }
-            .coordinateSpace(name: "scroll")
-            .navigationTitle("Scroll offset: \(scrollPosition.y)")
-            .navigationBarTitleDisplayMode(.inline)
-//            .scaledToFit()
         }
     }
 }
@@ -56,7 +52,5 @@ struct Video_Previews: PreviewProvider {
 
 struct ScrollOffsetPreferenceKey: PreferenceKey {
     static var defaultValue: CGPoint = .zero
-
-    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {
-    }
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
 }
